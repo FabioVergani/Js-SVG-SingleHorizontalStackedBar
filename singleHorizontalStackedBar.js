ac2d490a878ea0907008dd97e4c0190fc03e781c
Fabio Vergani <fabio.vergani.1975@gmail.com>
2020-12-07 14:50:44 +0100

    Create singleHorizontalStackedBar.js

---
+++ singleHorizontalStackedBar.js
@@ -0,0 +1,34 @@
+const StackedBar = (w=>{
+	const d=w.document,
+	frag=d.createDocumentFragment(),
+	nestSvgEl=(node,tag,attrs)=>{
+		const e=node.appendChild(d.createElementNS('http://www.w3.org/2000/svg',tag));
+		if(attrs){
+			for(const attr of w.Object.entries(attrs)){
+				e.setAttribute(...attr)
+			}
+		};
+		return e
+	};
+	return ({data,width=100,height=12})=>{
+		const sum=data.reduce((a,[b])=>a+b,0);
+		if(sum){
+			const svg=nestSvgEl(frag,'svg',{width,height,viewBox:`0 0 ${width} ${height}`});
+			let i=0;
+			for(const [value,fill,stroke] of data){
+				if(value){
+					const n=(value/sum*width);
+					nestSvgEl(svg,'rect',{
+						x:i.toFixed(2),
+						width:n.toFixed(2),
+						height,
+						fill,
+						stroke
+					});
+					i+=n
+				}
+			};
+			return svg
+		}
+	}
+})(window);
